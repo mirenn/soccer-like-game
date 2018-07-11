2ca2afe1819cb315e800fa596db2434a69e65109
nagai <nagai.hayate.76r@st.kyoto-u.ac.jp>
2018-07-12 03:08:11 +0900

    Photon Part4 done
    二人までのプレイヤーが接続したときのシーン遷移が可能になった。Masterクライアントになった場合、Loading Level:数字で接続した人数を表示するようになった。

---
+++ Assets/Scripts/RoomforX/GameManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace SoccerLikeGame
+{
+    public class GameManager : Photon.PunBehaviour
+    {
+
+        #region Photon Messages
+
+        /// <summary>
+        /// Called when the local player left the room. We need to load the launcher scene.
+        /// </summary>
+        public void OnLeftRoom()
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void LeaveRoom()
+        {
+            PhotonNetwork.LeaveRoom();
+        }
+
+        #endregion
+        #region Private Methods
+
+        void LoadArena()
+        {
+            //TODO:三人以上も許容しているがシーンを用意していない。許容するプレーヤー数を変更しよう
+            if (!PhotonNetwork.isMasterClient)
+            {
+                Debug.LogError("PhotonNetwork : Trying to Load a level but we are not the master Client");
+            }
+            Debug.Log("PhotonNetwork : Loading Level : " + PhotonNetwork.room.PlayerCount);
+            PhotonNetwork.LoadLevel("Roomfor" + PhotonNetwork.room.PlayerCount);//Roomforの間のスペースを除いた
+
+        }
+
+        #endregion
+
+        #region Photon Messages
+
+        public override void OnPhotonPlayerConnected(PhotonPlayer other)
+        {
+            Debug.Log("OnPhotonPlayerConnected() " + other.NickName); // not seen if you're the player connecting
+
+            if (PhotonNetwork.isMasterClient)
+            {
+                Debug.Log("OnPhotonPlayerConnected isMasterClient " + PhotonNetwork.isMasterClient); // called before OnPhotonPlayerDisconnected
+
+                LoadArena();
+            }
+        }
+
+        public override void OnPhotonPlayerDisconnected(PhotonPlayer other)
+        {
+            Debug.Log("OnPhotonPlayerDisconnected() " + other.NickName); // seen when other disconnects
+
+            if (PhotonNetwork.isMasterClient)
+            {
+                Debug.Log("OnPhotonPlayerConnected isMasterClient " + PhotonNetwork.isMasterClient); // called before OnPhotonPlayerDisconnected
+
+                LoadArena();
+            }
+        }
+
+        #endregion
+    }
+} 
+
