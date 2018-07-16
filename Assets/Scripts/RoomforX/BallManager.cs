f5e393e0afc0810c380802575259993b4ade00ae
nagai <nagai.hayate.76r@st.kyoto-u.ac.jp>
2018-07-17 01:49:52 +0900

    サンプルシーンにゴールを設置
    BallManager
    ボールがゴールに接触したら位置リセット

---
+++ Assets/Scripts/RoomforX/BallManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Goal")
+        {
+            Debug.Log("Goal");
+            transform.position = new Vector3(0f, 5f, 0f);
+        }
+    }
+}
