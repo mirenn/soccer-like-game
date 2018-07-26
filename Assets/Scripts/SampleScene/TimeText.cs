84632fd74decda76c18ccac9b7cbc41896f4f45b
nagai <nagai.hayate.76r@st.kyoto-u.ac.jp>
2018-07-27 01:55:24 +0900

    時間を同期できるようにした
    TimeTextを追加

--- Assets/Scripts/SampleScene/TimeText.cs
+++ Assets/Scripts/SampleScene/TimeText.cs
@@ -3,21 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class TimeText : MonoBehaviour {
+public class TimeText : Photon.MonoBehaviour {
 
 
     [SerializeField]
     private int minute;
     [SerializeField]
-    private float seconds;
+    public float seconds;
     //　前のUpdateの時の秒数
     private float oldSeconds;
     //　タイマー表示用テキスト
     private Text timerText;
 
     [SerializeField]
-    private int secondsTimeLimit; 
+    private int secondsTimeLimit;
 
+
+    #region monobehaviorメソッド
     // Use this for initialization
     void Start () {
         minute = 0;
@@ -29,11 +31,25 @@ public class TimeText : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+        if (PhotonNetwork.isMasterClient == false)
+        {
+            //　値が変わった時だけテキストUIを更新
+            if ((int)seconds != (int)oldSeconds)
+            {
+                timerText.text = "Time " + minute.ToString("00") + ":" + ((int)seconds).ToString("00");
+            }
+
+            return;
+        }
+        Debug.Log("マスターです");
+
+
         if(secondsTimeLimit <= seconds)
         {
             Debug.Log("時間になりました");
             return;
         }
+
 
         seconds += Time.deltaTime;
         if (seconds >= 60f)
@@ -44,8 +60,25 @@ public class TimeText : MonoBehaviour {
         //　値が変わった時だけテキストUIを更新
         if ((int)seconds != (int)oldSeconds)
         {
-            timerText.text = minute.ToString("00") + ":" + ((int)seconds).ToString("00");
+            timerText.text = "Time " + minute.ToString("00") + ":" + ((int)seconds).ToString("00");
         }
         oldSeconds = seconds;
     }
+    #endregion
+
+    #region Photon メソッド
+    void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            //データの送信
+            stream.SendNext(seconds);
+        }
+        else
+        {
+            //データの受信
+            this.seconds = (float)stream.ReceiveNext();
+        }
+    }
+    #endregion
 }
