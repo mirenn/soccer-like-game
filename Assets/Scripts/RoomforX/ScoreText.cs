58c400e82558badba3a0443b821b96af05ec68a9
nagai <nagai.hayate.76r@st.kyoto-u.ac.jp>
2018-07-27 02:34:47 +0900

    スコア同期の準備１
    スコアスクリプト用意
    ゴール側のスクリプト作成

---
+++ Assets/Scripts/RoomforX/ScoreText.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreText : MonoBehaviour {
+
+    private int positiveXScore;
+    private int negativeXScore;
+
+    #region Monobehaviourメソッド
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    #endregion
+
+    #region publicメソッド
+    public void AddPositiveXScore()
+    {
+        positiveXScore++;
+    }
+    public void AddNegativeXScore()
+    {
+        negativeXScore++;
+    }
+    #endregion
+}
